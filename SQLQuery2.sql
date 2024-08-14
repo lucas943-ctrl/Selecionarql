d06e917774504117f22b7cad10f52cdc4815e24d
lucas943-ctrl <80469445+lucas943-ctrl@users.noreply.github.com>
2024-08-14 06:54:38 -0300

    Update SQLQuery2.sql

--- SQLQuery2.sql
+++ SQLQuery2.sql
@@ -1,40 +1,5 @@
-/*
-CREATE TABLE d_calendario(
-	 datahora datetime
-	,data date
-	,intervalo  time);
 
-CREATE TABLE d_usuarios(
-	  id_usuario varchar(max)
-	 ,nome_usuario varchar(max));
 
-CREATE TABLE d_log_fontea(
-	  id_usuario varchar(max)
-	 ,evento varchar(max)
-	 ,inicio datetime
-	 ,fim datetime);
-
-CREATE TABLE d_log_fonteb(
-	  id_usuario varchar(max)
-	 ,evento varchar(max)
-	 ,inicio datetime
-	 ,fim datetime);
-
-CREATE TABLE f_log_v2(
-	  id_usuario varchar(max)
-	 ,evento varchar(max)
-	 ,inicio datetime
-	 ,fim datetime
-	 ,duracao time);
-
-CREATE TABLE r_resumo(
-	  data date
-	 ,intervalo time
-	 ,id_usuario varchar(max)
-	 ,nome varchar(max)
-	 ,duracao_evento_a time
-	 ,duracao_evento_b time);
-	 
 
 DECLARE @contador INT = 1;
 while @contador <= 10
