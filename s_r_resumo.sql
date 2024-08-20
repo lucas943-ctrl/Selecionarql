8d93eb47e9298d8d7b1b946dd28e7f0e263407ba
lucas943-ctrl <80469445+lucas943-ctrl@users.noreply.github.com>
2024-08-20 13:19:56 -0300

    Add files via upload

---
+++ s_r_resumo.sql
@@ -0,0 +1,11 @@
+USE db_dev
+
+--somar o tempo de log das duas tabelas
+--LEFT JOIN 
+
+SELECT <select_list>
+FROM Tabela A
+LEFT JOIN Tabela B
+ON A.Key = B.Key
+
+--inserir o log entre os intervalos
