85f320436e8c27ab9c0809541b8ab069a829992d
lucas943-ctrl <80469445+lucas943-ctrl@users.noreply.github.com>
2024-08-21 08:11:16 -0300

    Add files via upload

--- s_r_resumo.sql
+++ s_r_resumo.sql
@@ -3,9 +3,25 @@ USE db_dev
 --somar o tempo de log das duas tabelas
 --LEFT JOIN 
 
-SELECT <select_list>
-FROM Tabela A
-LEFT JOIN Tabela B
-ON A.Key = B.Key
+SELECT 
+	
+	intervalo
+	,contagem = COUNT(*)
+	,data 
+	,duracao = SUM(duracao)
+
+FROM d_calendario FULL JOIN f_log
+ON f_log.inicio <= CAST(d_calendario.data AS DATETIME) + CAST(d_calendario.intervalo AS DATETIME)
+AND f_log.fim >= CAST(d_calendario.data AS DATETIME) + CAST(d_calendario.intervalo AS DATETIME)
+
+WHERE data >= '01/07/2024'
+AND intervalo >= '08:00'
+AND intervalo <= '21:00'
+
+
+GROUP BY intervalo , data 
+ORDER BY data, intervalo
+
 
 --inserir o log entre os intervalos
+
