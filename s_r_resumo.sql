c3837ab2761ddf9caf75aab4011e82c7541a61da
lucas943-ctrl <80469445+lucas943-ctrl@users.noreply.github.com>
2024-08-22 13:37:24 -0300

    Update s_r_resumo.sql

--- s_r_resumo.sql
+++ s_r_resumo.sql
@@ -1,53 +1,49 @@
 USE db_dev
 
---somar o tempo de log das duas tabelas
---LEFT JOIN 
+GO
+
+CREATE OR ALTER PROCEDURE sp_att_r_resumo AS
 
 SELECT * FROM(SELECT 
 	
-	
-	 contagem = COUNT(*)
-	,data 
-	,id_usuario
-	,inicio
-	,fim
+	data 
 	,periodo_inicio 
 	,periodo_fim 
-	,duracao_condicao1 = SUM(IIF(CAST(inicio AS TIME) < intervalo AND CAST(fim AS TIME) > intervalo AND CAST(fim AS TIME) < DATEADD(minute, 30, intervalo), DATEDIFF(MINUTE, intervalo, CAST(fim AS TIME)), 0))
-	,duracao_condicao2 = SUM(IIF(CAST(inicio AS TIME) > intervalo AND CAST(fim AS TIME) < DATEADD(minute, 30, intervalo), duracao, 0))
-	,duracao_condicao3 = SUM(IIF(CAST(inicio AS TIME) > intervalo AND CAST(inicio AS TIME) < DATEADD(minute, 30, intervalo) AND CAST(fim AS TIME) > DATEADD(minute, 30, intervalo) , DATEDIFF(MINUTE,CAST(inicio AS TIME),DATEADD(minute, 30, intervalo)), 0))
-	,duracao_condicao4 = SUM(IIF(CAST(inicio AS TIME) < intervalo AND CAST(fim AS TIME) > DATEADD(minute, 30, intervalo), 30 , 0) )
-	,duracao = SUM(CASE
-	    WHEN CAST(inicio AS TIME) < intervalo AND CAST(fim AS TIME) > intervalo AND CAST(fim AS TIME) < DATEADD(minute, 30, intervalo)	THEN DATEDIFF(MINUTE, intervalo, CAST(fim AS TIME))
-		WHEN CAST(inicio AS TIME) > intervalo AND CAST(fim AS TIME) < DATEADD(minute, 30, intervalo)	THEN duracao
-		WHEN CAST(inicio AS TIME) > intervalo AND CAST(inicio AS TIME) < DATEADD(minute, 30, intervalo) AND CAST(fim AS TIME) > DATEADD(minute, 30, intervalo)								THEN DATEDIFF(MINUTE,CAST(inicio AS TIME),DATEADD(minute, 30, intervalo))
-		WHEN CAST(inicio AS TIME) < intervalo AND CAST(fim AS TIME) > DATEADD(minute, 30, intervalo)	THEN 30
+	, almoço = SUM(IIF(fonte = 'd_log_fontea', CASE
+	    WHEN CAST(inicio AS TIME) <= periodo_inicio AND CAST(fim AS TIME) > periodo_inicio AND CAST(fim AS TIME) < DATEADD(minute, 30, periodo_inicio)	THEN DATEDIFF(MINUTE, periodo_inicio, CAST(fim AS TIME))
+		WHEN CAST(inicio AS TIME) >= periodo_inicio AND CAST(fim AS TIME) <= DATEADD(minute, 30, periodo_inicio)	THEN duracao
+		WHEN CAST(inicio AS TIME) > periodo_inicio AND CAST(inicio AS TIME) <= DATEADD(minute, 30, periodo_inicio) AND CAST(fim AS TIME) > DATEADD(minute, 30, periodo_inicio)	THEN DATEDIFF(MINUTE,CAST(inicio AS TIME),DATEADD(minute, 30, periodo_inicio))
+		WHEN CAST(inicio AS TIME) <= periodo_inicio AND CAST(fim AS TIME) >= DATEADD(minute, 30, periodo_inicio)	THEN 30
 		ELSE 0
-		END	)
+		END, 0))
+
+	,lanche = SUM(IIF(fonte = 'd_log_fonteb', CASE
+	    WHEN CAST(inicio AS TIME) <= periodo_inicio AND CAST(fim AS TIME) > periodo_inicio AND CAST(fim AS TIME) < DATEADD(minute, 30, periodo_inicio)	THEN DATEDIFF(MINUTE, periodo_inicio, CAST(fim AS TIME))
+		WHEN CAST(inicio AS TIME) >= periodo_inicio AND CAST(fim AS TIME) <= DATEADD(minute, 30, periodo_inicio)	THEN duracao
+		WHEN CAST(inicio AS TIME) > periodo_inicio AND CAST(inicio AS TIME) <= DATEADD(minute, 30, periodo_inicio) AND CAST(fim AS TIME) > DATEADD(minute, 30, periodo_inicio)	THEN DATEDIFF(MINUTE,CAST(inicio AS TIME),DATEADD(minute, 30, periodo_inicio))
+		WHEN CAST(inicio AS TIME) <= periodo_inicio AND CAST(fim AS TIME) >= DATEADD(minute, 30, periodo_inicio)	THEN 30
+		ELSE 0
+		END, 0)	)
 
 FROM (
 	SELECT
 	 data
 	,periodo_inicio = intervalo
 	,periodo_fim = DATEADD(minute, 30, intervalo) 
-	FROM d_calendario
-)calendario
-FULL JOIN f_log
-ON f_log.inicio <= CAST(calendario.data AS DATETIME) + CAST(calendario.periodo_inicio AS DATETIME)
-AND f_log.fim >= CAST(calendario.data AS DATETIME) + CAST(calendario.periodo_inicio AS DATETIME)
+	 FROM d_calendario
+	  )calendario
 
-GROUP BY periodo_inicio , data, id_usuario, inicio, fim
+FULL JOIN f_log
+	ON f_log.inicio <= CAST(calendario.data AS DATETIME) + CAST(calendario.periodo_inicio AS DATETIME)
+	AND f_log.fim >= CAST(calendario.data AS DATETIME) + CAST(calendario.periodo_inicio AS DATETIME)
+
+GROUP BY periodo_inicio, periodo_fim ,  data
 )_
 
 WHERE data >= '01/07/2024'
---AND duracao_condicao1 > 0 
-AND periodo_inicio <= '21:00'
-AND id_usuario = 'C000008A'
-
-
 
 ORDER BY data, periodo_inicio
 
+GO
 
---inserir o log entre os intervalos
-
+EXECUTE sp_att_r_resumo
