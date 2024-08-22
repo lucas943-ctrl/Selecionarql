4fa653ca0f28d8a36d3aa766c394e78f370c20c1
lucas943-ctrl <80469445+lucas943-ctrl@users.noreply.github.com>
2024-08-22 13:34:27 -0300

    Update Gerador de calendario.sql

--- Gerador de calendario.sql
+++ Gerador de calendario.sql
@@ -1,12 +1,17 @@
+USE db_dev
+
+GO
+
 delete from d_calendario
 
-go 
+GO
 
-CREATE OR ALTER PROCEDURE sp_Calendarioneitor AS
+CREATE OR ALTER PROCEDURE s_calendario AS
 
-DECLARE @data_atual DATETIME = '01/06/2024 00:30'; 
+DECLARE @data_atual DATETIME = '01/06/2024 08:00'; 
 
 WHILE @data_atual <= '31/07/2024'
+
 BEGIN 
     INSERT INTO d_calendario(datahora, data, intervalo)
     VALUES 
@@ -15,14 +20,21 @@ BEGIN
 		 ,CAST(@data_atual AS DATE)
 		 ,DATEADD(SECOND, DATEDIFF(SECOND, CAST(@data_atual AS DATE), @data_atual),'00:00')
     );
+ 
+	SET @data_atual = DATEADD(MINUTE, 30, @data_atual); 
+	IF @data_atual < DATEADD(MINUTE, 8*60, CAST(CAST(@data_atual AS DATE) AS DATETIME) )
+		SET @data_atual = DATEADD(MINUTE, 8*60, CAST(CAST(@data_atual AS DATE) AS DATETIME) )
 
-    --SET @data_atual = DATEADD(DAY, 1, @data_atual); 
-	  SET @data_atual = DATEADD(MINUTE, 30, @data_atual); 
+	ELSE IF @data_atual > DATEADD(MINUTE, 21*60, CAST(CAST(@data_atual AS DATE) AS DATETIME) )
+		SET @data_atual = DATEADD(MINUTE, 8*60 + 24*60, CAST(CAST(@data_atual AS DATE) AS DATETIME) )
+
+
 END;
 
 GO
-Execute sp_Calendarioneitor
+Execute s_calendario
 
 GO
 
-SELECT * FROM d_calendario;
+SELECT * FROM d_calendario
+ORDER BY datahora
