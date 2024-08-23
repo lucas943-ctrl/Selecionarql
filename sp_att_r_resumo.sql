c1f8b836ee9e83b56d8462820d05ce829e89c71e
lucas943-ctrl <80469445+lucas943-ctrl@users.noreply.github.com>
2024-08-23 13:25:46 -0300

    Update sp_att_r_resumo.sql

--- sp_att_r_resumo.sql
+++ sp_att_r_resumo.sql
@@ -1,14 +1,20 @@
 USE db_dev
 
+DELETE r_resumo
+
 GO
 
 CREATE OR ALTER PROCEDURE sp_att_r_resumo AS
 
+INSERT INTO r_resumo
+
 SELECT * FROM(SELECT 
-	
-	data 
+	 data  
 	,periodo_inicio 
-	,periodo_fim 
+	,f_log.id_usuario
+	,nome_usuario
+
+
 	, almoço = SUM(IIF(fonte = 'd_log_fontea', CASE
 	    WHEN CAST(inicio AS TIME) <= periodo_inicio AND CAST(fim AS TIME) > periodo_inicio AND CAST(fim AS TIME) < DATEADD(minute, 30, periodo_inicio)	THEN DATEDIFF(MINUTE, periodo_inicio, CAST(fim AS TIME))
 		WHEN CAST(inicio AS TIME) >= periodo_inicio AND CAST(fim AS TIME) <= DATEADD(minute, 30, periodo_inicio)	THEN duracao
@@ -37,13 +43,19 @@ FULL JOIN f_log
 	ON f_log.inicio <= CAST(calendario.data AS DATETIME) + CAST(calendario.periodo_inicio AS DATETIME)
 	AND f_log.fim >= CAST(calendario.data AS DATETIME) + CAST(calendario.periodo_inicio AS DATETIME)
 
-GROUP BY periodo_inicio, periodo_fim ,  data
+LEFT JOIN d_usuarios
+ON  f_log.id_usuario = d_usuarios.id_usuario  
+
+GROUP BY periodo_inicio, periodo_fim , f_log.id_usuario,d_usuarios.nome_usuario,  data
 )_
 
 WHERE data >= '01/07/2024'
+AND  nome_usuario IS NOT NULL
 
 ORDER BY data, periodo_inicio
 
 GO
 
 EXECUTE sp_att_r_resumo
+
+SELECT * FROM r_resumo
