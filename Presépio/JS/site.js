41cfe7f9366c7b603540fbfe967b08adc601ad14
Lucas B. <80469445+lucas943-ctrl@users.noreply.github.com>
2024-11-28 15:26:53 -0300

    Add files via upload

--- Presépio/JS/site.js
+++ Presépio/JS/site.js
@@ -1,17 +1,18 @@
 function Expandir(ocultarid, maisid){
-	//var div=document.getElementById();
-	var ocultar=document.getElementById(ocultarid);
 	var maisTexto=document.getElementById(maisid);
 
 	
 
-	if(ocultar.style.display === "none"){
-		ocultar.style.display="inline";
-		maisTexto.style.display="none";
+	if(maisTexto.style.display === "none"){
+		maisTexto.style.display="flex";
 		
 	} else{
-		ocultar.style.display="none";
-		maisTexto.style.display="inline";
+		maisTexto.style.display="none";
 		
 }
+}
+
+
+	
+	
 }
